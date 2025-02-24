deff51e6e46f432e18c0dc867a054ff6297dceb6
AN-UNKNOWN <anuragpindoriya@gmail.com>
2025-02-24 16:33:19 +0530

    add routing and authentication to main application with Redux integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {Button, Layout, Menu} from "antd";
+import {logout} from "./store/authSlice";
+import Dashboard from "./pages/Dashboard";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import {useEffect} from "react";
+import {addToCart, addToWishlist, updatePropertyOfProduct} from "./store/productSlice.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+const {Header, Content} = Layout;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    const {isAuthenticated} = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    // this get value of product from local storage and update product
+    useEffect(() => {
+        const cartValue = JSON.parse(localStorage.getItem('cart'));
+        const wishlistValue = JSON.parse(localStorage.getItem('wishlist'));
 
-export default App
+        cartValue.forEach((item) => {
+            dispatch(addToCart(item));
+            dispatch(updatePropertyOfProduct({id: item.id, property: "isInCart", value: true}));
+        });
+
+        wishlistValue.forEach((item) => {
+            dispatch(addToWishlist(item));
+            dispatch(updatePropertyOfProduct({id: item.id, property: "isInWishlist", value: true}));
+        });
+
+    }, [dispatch]);
+
+    return (
+        <Router>
+            <Layout>
+                <Header>
+                    <Menu theme="dark" mode="horizontal" className="flex justify-between">
+                        <div>
+                            <Menu.Item key="1"><Link to="/">Dashboard</Link></Menu.Item>
+                            <Menu.Item key="2"><Link to="/cart">Cart</Link></Menu.Item>
+                            <Menu.Item key="3"><Link to="/wishlist">Wishlist</Link></Menu.Item>
+                        </div>
+                        <div>
+                            {isAuthenticated ? (
+                                <Button onClick={() => dispatch(logout())}>Logout</Button>
+                            ) : (
+                                <Link to="/login"><Button>Login</Button></Link>
+                            )}
+                        </div>
+
+                    </Menu>
+                </Header>
+                <Content style={{padding: "20px"}}>
+                    <Routes>
+                        <Route path="/" element={<Dashboard/>}/>
+                        <Route path="/login" element={<Login/>}/>
+
+                        {/* Protected Routes */}
+                        <Route path="/cart" element={<ProtectedRoute><Cart/></ProtectedRoute>}/>
+                        <Route path="/wishlist" element={<ProtectedRoute><Wishlist/></ProtectedRoute>}/>
+                    </Routes>
+                </Content>
+            </Layout>
+        </Router>
+    );
+};
+
+export default App;
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
-
+import productReducer from "./productSlice";
+import authReducer from "./authSlice";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        products: productReducer,
+        auth: authReducer,
+    },
 });
