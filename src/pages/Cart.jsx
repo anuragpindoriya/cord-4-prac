3d73032d983178e447ac6e10496f2cc2d5cf6edd
AN-UNKNOWN <anuragpindoriya@gmail.com>
2025-02-24 18:25:56 +0530

    feat: enhance cart functionality with total amount and product count display, add purchase reset feature

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,6 +9,8 @@ const Cart = () => {
     const dispatch = useDispatch();
     const products = useSelector((state) => state.products.products);
     const cartProduct = useMemo(() => products?.filter((item) => item.isInCart), [products]);
+    const totalAmount = useMemo(() => cartProduct.reduce((acc, item) => acc + item.price, 0), [cartProduct]);
+    const totalProduct = useMemo(() => cartProduct.length, [cartProduct]);
     const {isAuthenticated} = useSelector((state) => state.auth);
 
     if (!isAuthenticated) return <Navigate to="/login"/>;
@@ -16,21 +18,36 @@ const Cart = () => {
         dispatch(addToCart(product));
         toast.success("Product remove from cart");
     }
+    const resetCart = () => {
+        cartProduct.forEach((product) => {
+            dispatch(addToCart(product));
+        })
 
+        toast.success('Thank you for purchase ')
+    }
     return (
-        <div>
-            <h2>Cart</h2>
-            <div style={{display: "flex", gap: "20px", flexWrap: "wrap"}}>
-                {cartProduct.map((product) => (
-                    <Card key={product.id} title={product.title} style={{width: 300}}>
-                        <img src={product.image} alt={product.title}/>
-                        <p>Price: ${product.price}</p>
-                        <Button type={"primary"}
-                                onClick={() => handelRemoveFromCart(product)}>Remove from cart</Button>
+        <div className={'flex gap-[20px]'}>
+            {totalProduct > 0 ? <div>
+                <h2>Cart</h2>
+                <div style={{display: "flex", gap: "20px", flexWrap: "wrap"}}>
+                    {cartProduct.map((product) => (
+                        <Card key={product.id} title={product.title} style={{width: 300}}>
+                            <img src={product.image} alt={product.title}/>
+                            <p>Price: ${product.price}</p>
+                            <Button type={"primary"}
+                                    onClick={() => handelRemoveFromCart(product)}>Remove from cart</Button>
 
-                    </Card>
-                ))}
-            </div>
+                        </Card>
+                    ))}
+                </div>
+            </div> : <div><p className={'text-[40px]'}>No product in cart</p></div>}
+            {totalProduct > 0 && (<div className={'h-[20px] py-[20px] flex flex-col gap-[10px]'}>
+                <p className={'text-[20px]'}>Total number of product:- {totalProduct}</p>
+                <p className={'text-[20px]'}>Total amount:- ${totalAmount}</p>
+                <div>
+                    <Button type={'primary'} onClick={resetCart}>Buynow</Button>
+                </div>
+            </div>)}
         </div>
     );
 };
